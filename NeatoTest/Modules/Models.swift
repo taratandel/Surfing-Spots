39bb33a2dee9b740bb50f0ce42f08e936cd491d2
Tara Tandel <tara.tandel@icloud.com>
2020-08-19 04:22:13 +0430

    Add CoreData
    Because it is possible that we want to save the name of the cities inside the core data I added save and retrieve funcs

--- NeatoTest/Modules/Models.swift
+++ NeatoTest/Modules/Models.swift
@@ -7,3 +7,68 @@
 //
 
 import Foundation
+import CoreData
+import UIKit
+
+class citiesCoreData {
+    var cities: [NSManagedObject] = []
+    
+    func saveToCoreData(name: String) {
+        guard let appDelegate =
+            UIApplication.shared.delegate as? AppDelegate else {
+                return
+        }
+        
+        // 1
+        let managedContext =
+            appDelegate.persistentContainer.viewContext
+        
+        // 2
+        let entity =
+            NSEntityDescription.entity(forEntityName: "Cities",
+                                       in: managedContext)!
+        
+        let person = NSManagedObject(entity: entity,
+                                     insertInto: managedContext)
+        
+        // 3
+        person.setValue(name, forKeyPath: "name")
+        
+        // 4
+        do {
+            try managedContext.save()
+            cities.append(person)
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+    }
+    
+    func retrieveFromeCoreData() {
+        guard let appDelegate =
+            UIApplication.shared.delegate as? AppDelegate else {
+                return
+        }
+        
+        // 1
+        let managedContext =
+            appDelegate.persistentContainer.viewContext
+        let fetchRequest =
+            NSFetchRequest<NSManagedObject>(entityName: "Person")
+        
+        //3
+        do {
+            cities = try managedContext.fetch(fetchRequest)
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    }
+    
+}
+
+struct City: Codable {
+    var name: String?
+}
+
+struct CitiesDic: Codable {
+    var cities:[City]
+}
