9ef4fe7ef578501f35e1689f5c808e1935dd6d15
Tara Tandel <tara.tandel@icloud.com>
2020-08-18 12:05:51 +0430

    Initial Commit

---
+++ NeatoTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NeatoTest
+//
+//  Created by Tara Tandel on 18/08/2020.
+//  Copyright © 2020 Tara Tandel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NeatoTestTests/NeatoTestTests.swift
@@ -0,0 +1,34 @@
+//
+//  NeatoTestTests.swift
+//  NeatoTestTests
+//
+//  Created by Tara Tandel on 18/08/2020.
+//  Copyright © 2020 Tara Tandel. All rights reserved.
+//
+
+import XCTest
+@testable import NeatoTest
+
+class NeatoTestTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
