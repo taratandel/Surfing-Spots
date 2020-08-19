2f424842de88ed089016e08c8fdb6ca77feb3fb2
Tara Tandel <tara.tandel@icloud.com>
2020-08-19 16:07:43 +0430

    Add collection view to viewController
    I added the collection view delegate and data source to the view and made connection between the collection view and the presenter. the implementation is still not complete

--- NeatoTest/ViewController.swift
+++ NeatoTest/ViewController.swift
@@ -15,9 +15,16 @@ class ViewController: BaseViewController {
     
     
     var presenter: PresenterProtocol?
+    
+    private let itemsPerRow = 1
+    private let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+    
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         showIndicatorView(with: "Wait :/")
+        setupCollectionView()
         // Do any additional setup after loading the view.
     }
 
@@ -31,4 +38,65 @@ class ViewController: BaseViewController {
         presenter?.mainViewDidLoad()
 
     }
+     func setupCollectionView() {
+            (cityCollectionView.collectionViewLayout as! UICollectionViewFlowLayout).estimatedItemSize = UICollectionViewFlowLayout.automaticSize
+            (cityCollectionView.collectionViewLayout as! UICollectionViewFlowLayout).sectionInsetReference = .fromLayoutMargins
+
+            cityCollectionView.contentInsetAdjustmentBehavior = .always
+            
+            cityCollectionView?.delegate = self
+            cityCollectionView?.dataSource = self
+        }
+        
+    }
+
+    // MARK: - UICollectionViewDelegate
+    extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+            return presenter?.getTheNumberOfItemsInSection(section: section) ?? 0
+        }
+        
+        func numberOfSections(in collectionView: UICollectionView) -> Int {
+            return 1
+        }
+        
+        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+            let gestureCell = cityCollectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CityCollectionViewCell
+            guard let cityDataToBuild = presenter?.getCity(at: indexPath) else {
+                return UICollectionViewCell()
+            }
+            gestureCell.fillData(cityDataToBuild)
+            return gestureCell
+        }
+    }
+
+    // MARK: - UICollectionViewDelegateFlowLayout
+    extension ViewController: UICollectionViewDelegateFlowLayout {
+        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+            if indexPath == largePhotoIndexPath {
+                var size = collectionView.bounds.size
+                size.height -= (sectionInsets.top + sectionInsets.right)
+                size.width -= (sectionInsets.left + sectionInsets.right)
+                return size
+            }
+
+            let paddingSpace = sectionInsets.left * CGFloat(itemsPerRow + 1)
+            let availableWidth = view.frame.width - paddingSpace
+            let widthPerItem = availableWidth / CGFloat(itemsPerRow)
+
+            return CGSize(width: widthPerItem, height: widthPerItem)
+        }
+
+        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+            return sectionInsets
+        }
+
+        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+            return sectionInsets.left
+        }
+
+    }
+
     
+
+
