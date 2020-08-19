19fb7c2001957c18cb3e174ac1cd7ac5be57e7e0
Tara Tandel <tara.tandel@icloud.com>
2020-08-19 14:30:23 +0430

    Add protocol for fetching the result of the netwrok

---
+++ NeatoTest/Common/CommonProtocols.swift
@@ -0,0 +1,16 @@
+//
+//  CommonProtocols.swift
+//  NeatoTest
+//
+//  Created by Tara Tandel on 19/08/2020.
+//  Copyright © 2020 Tara Tandel. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+/// For fetching the result of the service
+protocol RequestServices: class {
+    func requestIsComplete(_ response: Data)
+    func requestFaild(_ error: Error)
+}
+
