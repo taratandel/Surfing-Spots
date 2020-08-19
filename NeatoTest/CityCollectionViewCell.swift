2e8e317fe5f3042987a08ff6edbbe72a97273f98
Tara Tandel <tara.tandel@icloud.com>
2020-08-19 16:10:39 +0430

    Implement the collection view cell
    This commit will creat a cell with the given data the collection view doesn’t have any knowledge about any other parts of the program

---
+++ NeatoTest/CityCollectionViewCell.swift
@@ -0,0 +1,53 @@
+//
+//  CityCollectionViewCell.swift
+//  NeatoTest
+//
+//  Created by Tara Tandel on 19/08/2020.
+//  Copyright © 2020 Tara Tandel. All rights reserved.
+//
+
+import UIKit
+import SDWebImage
+
+class CityCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var cityImage: UIImageView!
+    @IBOutlet weak var cityName: UILabel!
+    @IBOutlet weak var cityTemp: UILabel!
+    @IBOutlet weak var imageLoader: UIActivityIndicatorView!
+    
+    override var isSelected: Bool {
+        didSet {
+            cityImage.layer.borderWidth = isSelected ? 10 : 2
+            cityImage.layer.borderColor = isSelected ? UIColor.red.cgColor : UIColor.blue.cgColor
+            
+        }
+    }
+    
+    override func awakeFromNib() {
+        isSelected = false
+        super.awakeFromNib()
+    }
+    
+    func fillData (_ city: City) {
+        imageLoader.startAnimating()
+        self.cityImage.sd_imageIndicator = SDWebImageActivityIndicator.gray
+        self.cityImage.sd_setImage(with: URL(string: "https://images.app.goo.gl/AdXXAUfem1iPzB1x7"), placeholderImage: UIImage(named: "g1"), options: .progressiveLoad, completed: {
+            [weak self]  (_,_,_,_) in
+            self?.imageLoader.isHidden = true
+        })
+        self.cityImage.backgroundColor = .red
+        self.cityName.text = city.name
+        self.cityName.backgroundColor = .darkGray
+        self.cityName.textColor = .white
+        self.backgroundColor = .black
+    }
+    
+    override func preferredLayoutAttributesFitting(_ layoutAttributes: UICollectionViewLayoutAttributes) -> UICollectionViewLayoutAttributes {
+        layoutIfNeeded()
+        let layoutAttributes = super.preferredLayoutAttributesFitting(layoutAttributes)
+        layoutAttributes.bounds.size = systemLayoutSizeFitting(UIView.layoutFittingCompressedSize, withHorizontalFittingPriority: .required, verticalFittingPriority: .defaultLow)
+        return layoutAttributes
+    }
+    
+}
