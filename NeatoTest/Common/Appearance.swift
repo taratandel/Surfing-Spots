a3e6d029424798d830785ef967059f889da2cda2
Tara Tandel <tara.tandel@icloud.com>
2020-08-19 16:09:35 +0430

    Add some more appearance
    add a very cute color blue
    add a function which will remove the shadow of the navigation bar this will make the navigation part a part of the view

--- NeatoTest/Common/Appearance.swift
+++ NeatoTest/Common/Appearance.swift
@@ -60,3 +60,21 @@ extension UIFont {
         return UIFont(isBold: true, withSize: 15)
     }
 }
+
+extension UINavigationBar {
+    
+    func shouldRemoveShadow(_ value: Bool) -> Void {
+        if value {
+            self.setValue(true, forKey: "hidesShadow")
+        } else {
+            self.setValue(false, forKey: "hidesShadow")
+        }
+    }
+}
+
+extension UIColor {
+    static var lightBlue: UIColor {
+        return UIColor(red: 158/256, green: 224/256, blue: 255/256, alpha: 1)
+    }
+}
+
