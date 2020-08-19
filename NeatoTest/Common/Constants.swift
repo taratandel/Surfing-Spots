9e103e86c33338bd6ad64e1d07952e53f6f6c7e4
Tara Tandel <tara.tandel@icloud.com>
2020-08-19 19:10:10 +0430

    Add static url for getting the picture from a server

--- NeatoTest/Common/Constants.swift
+++ NeatoTest/Common/Constants.swift
@@ -13,3 +13,9 @@ struct EndPoints {
     static let randomServerEndPoint = "http://numbersapi.com"
     static let citiesSeverEndPoint = "https://run.mocky.io/v3"
 }
+
+struct PicURLs {
+    static let randomPicFresh = "https://picsum.photos/400/300?random=1.jpg"
+    static let randomPicUgly = "https://picsum.photos/400/300?random=1&grayscale&blur=2.jpg"
+
+}
