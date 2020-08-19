1c9826394643a12231fb8c826ea677395a7328b0
Tara Tandel <tara.tandel@icloud.com>
2020-08-19 15:57:00 +0430

    Add the implementation for request protocol
    This commit will implement the protocol function for requesting the data and also there is function which will interpret the error and shows the suitable alert

--- NeatoTest/Modules/Presenter.swift
+++ NeatoTest/Modules/Presenter.swift
@@ -44,4 +44,77 @@ class Presenter: PresenterProtocol {
     func getTheNumberOfItemsInSection(section: Int) -> Int {
         return 2
     }
-    
+    /**
+     After the view is completed the it will ask for data and the presenter is reponsilble for netwroking
+     */
+    func mainViewDidLoad() {
+        client?.getTheListData(url: RequestType.cityList.path, method: .get, parameter: nil, header: nil)
+    }
+}
+/// The necessary functions for networking after the request is complete the presenter is responsible for data convertion and also for the connection between the entity
+extension Presenter: RequestServices {
+    /**
+     The implementation of the protocol function when the request is complete
+     
+     based on the result of parsing the failure or success will be concluded
+     - Parameters:
+        - respnse: data type for parsing
+     */
+    func requestIsComplete(_ response: Data) {
+        do {
+            let parsedData = try JSONDecoder().decode(CitiesDic.self, from: response)
+            if parsedData.cities.count > 0 {
+                print(parsedData)
+            } else {
+                self.fetchFailed(error: RequestErrorType.serverError, message: "req failed")
+            }
+        }
+        catch {
+            self.fetchFailed(error: RequestErrorType.badRequest, message: nil)
+        }
+    }
+    /**
+     The implementation of the protocol function when the request fails
+     - Parameters:
+        - error: Error type when request fails
+     */
+    func requestFaild(_ error: Error) {
+        self.fetchFailed(error: error, message: nil)
+    }
+    /**
+     this function will show an alerts based on the result of the failed request
+     - Parameters:
+        - error: the Error type when there are an error
+        - message: the message that must be shown to the user
+     */
+    func fetchFailed(error: Error, message: String?) {
+        if message != nil {
+            switch error {
+            case RequestErrorType.badRequest:
+                let actions = [UIAlertAction(title: "Cancel", style: .cancel, handler: { (action) in
+                })]
+                view?.fetchFailed(title: "Request Error", message: message!, actions: actions)
+            case RequestErrorType.noInternet:
+                let actions = [UIAlertAction(title: "Retry", style: .cancel, handler: { [weak self](action) in
+                    self?.mainViewDidLoad()
+                })]
+                view?.fetchFailed(title: "NO Internet", message: message!, actions: actions)
+            case RequestErrorType.serverError:
+                let actions = [UIAlertAction(title: "Retry", style: .cancel, handler: { [weak self](action) in
+                    self?.mainViewDidLoad()
+                })]
+                view?.fetchFailed(title: "Server Error", message: message!, actions: actions)
+            default:
+                let actions = [UIAlertAction(title: "Cancel", style: .cancel, handler: {(action) in
+                })]
+                view?.fetchFailed(title: "Request Failed", message: message!, actions: actions)
+            }
+        } else {
+            let actions = [UIAlertAction(title: "Retry", style: .cancel, handler: { [weak self](action) in
+                self?.mainViewDidLoad()
+            })]
+            view?.fetchFailed(title: "Oops", message: error.localizedDescription, actions: actions)
+        }
+    }
+
+}
