04eebd9421ed1191dbaa66dc9ac2c21a8a368341
Tara Tandel <tara.tandel@icloud.com>
2020-08-19 14:27:58 +0430

    Add the endpoints of the servers as constants
    In case there are more APIs that uses one server endpoint this approach will become handy

---
+++ NeatoTest/Common/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  NeatoTest
+//
+//  Created by Tara Tandel on 18/08/2020.
+//  Copyright © 2020 Tara Tandel. All rights reserved.
+//
+
+import Foundation
+
+/// the endpoints of the servers
+struct EndPoints {
+    static let randomServerEndPoint = "http://numbersapi.com"
+    static let citiesSeverEndPoint = "https://run.mocky.io/v3"
+}
