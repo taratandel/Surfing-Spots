470757c2977d4aa8e988cd076da99c5e7bf4d1be
Tara Tandel <tara.tandel@icloud.com>
2020-08-20 01:32:36 +0430

    Add a function to change the temprature
    This implementation is not complete commited because of the change in context the overall work is like this :
    when ever a new temprature is available the method ‘changeTheTemprature’. will be called

--- NeatoTest/Modules/View/CityCollectionViewCell.swift
+++ NeatoTest/Modules/View/CityCollectionViewCell.swift
@@ -15,7 +15,7 @@ class CityCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var cityName: UILabel!
     @IBOutlet weak var cityTemp: UILabel!
     @IBOutlet weak var imageLoader: UIActivityIndicatorView!
-    
+    private var city: City
     override var isSelected: Bool {
         didSet {
             cityImage.layer.borderWidth = isSelected ? 10 : 2
@@ -30,17 +30,21 @@ class CityCollectionViewCell: UICollectionViewCell {
     }
     
     func fillData (_ city: City) {
+        self.city = city
         imageLoader.startAnimating()
         self.cityImage.sd_imageIndicator = SDWebImageActivityIndicator.gray
-        self.cityImage.sd_setImage(with: URL(string: "https://images.app.goo.gl/AdXXAUfem1iPzB1x7"), placeholderImage: UIImage(named: "g1"), options: .progressiveLoad, completed: {
-            [weak self]  (_,_,_,_) in
-            self?.imageLoader.isHidden = true
-        })
         self.cityImage.backgroundColor = .red
         self.cityName.text = city.name
         self.cityName.backgroundColor = .darkGray
         self.cityName.textColor = .white
         self.backgroundColor = .black
+    }
+    
+    func changeTheTemprature(_ temp: Int) {
+        self.cityImage.sd_setImage(with: URL(string: "https://images.app.goo.gl/AdXXAUfem1iPzB1x7"), placeholderImage: UIImage(named: "PlaceHolder"), options: .progressiveLoad, completed: {
+            [weak self]  (_,_,_,_) in
+            self?.imageLoader.isHidden = true
+        })
     }
     
     override func preferredLayoutAttributesFitting(_ layoutAttributes: UICollectionViewLayoutAttributes) -> UICollectionViewLayoutAttributes {
