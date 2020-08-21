587d4bc9d0ee98c4c98497a871acff4d46e1c889
Tara Tandel <tara.tandel@icloud.com>
2020-08-22 01:19:56 +0430

    Delay lunch screen
    according to HIG we should not interrupt the launch screen because of the fast loading. The app is very light so it was almost impossible to see the launch screen I delayed the loading

--- NeatoTest/AppDelegate.swift
+++ NeatoTest/AppDelegate.swift
@@ -15,7 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        /// sets the global appearance for the app 
+        /// sets the global appearance for the app
+        Thread.sleep(forTimeInterval: 1.0)
         Appearances.setGlobalAppearance()
 
         return true
